8fc4921d5b2cd15dc32ff82e7da06a0a2e58db9f
Mukul Varshney <72725782+mukuldeepfence@users.noreply.github.com>
2023-01-12 00:23:12 +0530

    fetching data

--- deepfence_server/handler/user.go
+++ deepfence_server/handler/user.go
@@ -21,29 +21,29 @@ const (
 	DefaultNamespace   = "default"
 )
 
-//type SecretDoc struct {
-//	Identity   int      `json:"identity"`
-//	Labels     []string `json:"labels"`
-//	Properties struct {
-//		FullFilename          string  `json:"full_filename"`
-//		ImageLayerID          string  `json:"ImageLayerId"`
-//		Level                 string  `json:"level"`
-//		KubernetesClusterName string  `json:"kubernetes_cluster_name"`
-//		Masked                string  `json:"masked"`
-//		StartingIndex         int     `json:"starting_index"`
-//		RelativeEndingIndex   int     `json:"relative_ending_index"`
-//		NodeName              string  `json:"node_name"`
-//		Score                 float64 `json:"score"`
-//		MatchedContent        string  `json:"matched_content"`
-//		NodeType              string  `json:"node_type"`
-//		Timestamp             string  `json:"@timestamp"`
-//		ContainerName         string  `json:"container_name"`
-//		ScanID                string  `json:"scan_id"`
-//		RelativeStartingIndex int     `json:"relative_starting_index"`
-//		HostName              string  `json:"host_name"`
-//		NodeID                string  `json:"node_id"`
-//	} `json:"properties"`
-//}
+type SecretDoc struct {
+	Identity   int      `json:"identity"`
+	Labels     []string `json:"labels"`
+	Properties struct {
+		FullFilename          string  `json:"full_filename"`
+		ImageLayerID          string  `json:"ImageLayerId"`
+		Level                 string  `json:"level"`
+		KubernetesClusterName string  `json:"kubernetes_cluster_name"`
+		Masked                string  `json:"masked"`
+		StartingIndex         int     `json:"starting_index"`
+		RelativeEndingIndex   int     `json:"relative_ending_index"`
+		NodeName              string  `json:"node_name"`
+		Score                 float64 `json:"score"`
+		MatchedContent        string  `json:"matched_content"`
+		NodeType              string  `json:"node_type"`
+		Timestamp             string  `json:"@timestamp"`
+		ContainerName         string  `json:"container_name"`
+		ScanID                string  `json:"scan_id"`
+		RelativeStartingIndex int     `json:"relative_starting_index"`
+		HostName              string  `json:"host_name"`
+		NodeID                string  `json:"node_id"`
+	} `json:"properties"`
+}
 
 func (h *Handler) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var registerRequest model.UserRegisterRequest
@@ -223,10 +223,10 @@ func (h *Handler) GenerateXlsxReport(w http.ResponseWriter, r *http.Request) {
 			continue
 		}
 
-		var secretDoc interface{}
-		secretDoc = record.Values[0]
+		secretDoc := record.Values[0].(SecretDoc)
+
 		fmt.Printf("%+v", secretDoc)
-		fmt.Println(secretDoc.Props.full_filename)
+		//fmt.Println(secretDoc.Props.full_filename)
 	}
 
 	httpext.JSON(w, http.StatusOK, model.Response{Success: true, Data: user})
