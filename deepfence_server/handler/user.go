86ffaf0d2464083cc13dba794fa2c352ce773c33
Mukul Varshney <72725782+mukuldeepfence@users.noreply.github.com>
2023-01-12 14:18:53 +0530

    adding excel sheet

--- deepfence_server/handler/user.go
+++ deepfence_server/handler/user.go
@@ -222,11 +222,13 @@ func (h *Handler) GenerateXlsxReport(w http.ResponseWriter, r *http.Request) {
 
 	secretDocHeader := map[string]string{"A1": "FileName", "B1": "ImageLayerId", "C1": "level", "D1": "kubernetes_cluster_name", "E1": "node_name", "F1": "Score", "G1": "Matched Content", "H1": "Node Type", "I1": "TimeStamp", "J1": "Host Name", "K": "Node Id"}
 
+	f.SetSheetName("Sheet1", "Secret")
+
 	for k, v := range secretDocHeader {
-		f.SetCellValue("SecretScan", k, v)
+		f.SetCellValue("Secret", k, v)
 	}
 
-	if err := f.SaveAs("/tmp/secret-scan.xlsx"); err != nil {
+	if err := f.SaveAs("/secret-scan.xlsx"); err != nil {
 		fmt.Println(err)
 	}
 
